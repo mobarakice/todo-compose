2c4685a85dfd9456e7b12a0765be1f9a9c7e9e5a
mobarak <mobarak.ice.06@gmail.com>
2023-12-21 00:18:27 +0600

    Modified message screen design

--- app/src/main/java/com/example/todocompose/ui/gemini/MessageScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/gemini/MessageScreen.kt
@@ -7,30 +7,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.exclude
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -41,7 +32,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -50,7 +40,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -91,7 +80,7 @@ fun MessageTopAppBar(openDrawer: () -> Unit) {
 fun MessageScreen(
     openDrawer: () -> Unit,
     viewModel: MessageViewModel? = null,
-    snackbarHostState: SnackbarHostState = SnackbarHostState()
+    snackBarHostState: SnackbarHostState = SnackbarHostState()
 ) {
     Scaffold(
         topBar = {
@@ -126,8 +115,139 @@ fun MessageScreen(
     }
 }
 
-fun onChange(string: String) {
+@Composable
+fun MessageInputBox(
+    modifier: Modifier = Modifier.fillMaxSize(),
+    message: String,
+    onMessageChanged: (String) -> Unit = {}
+) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .clickable {}
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.primary, shape = CircleShape)
+                .padding(
+                    horizontal = 16.dp,
+                )
+                .weight(1f)
+                .clickable {}
+        ) {
+            val textFieldColors = TextFieldDefaults.colors(
+                unfocusedPlaceholderColor = Color.LightGray,
+                focusedPlaceholderColor = Color.White,
+                unfocusedTextColor = Color.LightGray,
+                focusedTextColor = Color.White,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.White
+            )
+            var text by remember { mutableStateOf("") }
+            Icon(painterResource(id = R.drawable.tag_faces_black_24dp_1), null, tint = Color.White)
+            TextField(
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.ask_your_question_here),
+                        style = Typography.titleMedium
+                    )
+                },
+                textStyle = Typography.titleMedium.copy(fontWeight = FontWeight.Bold),
+                maxLines = 1,
+                colors = textFieldColors
+            )
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        Box(
+            modifier = Modifier
+                .size(56.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = CircleShape
+                )
+                .clickable {
 
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_microphone),
+                null, tint = Color.White
+            )
+        }
+    }
+}
+
+@Composable
+fun MessageItem(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            if (message.id == 1) {
+                MessageReceiveItem(item = message)
+            } else {
+                MessageSendItem(item = message)
+            }
+        }
+    }
+}
+
+fun getDummyData() = listOf(
+    Message("Hello", 5),
+    Message("Hello, how can I help you?", 1)
+)
+
+@Composable
+fun MessageSendItem(item: Message) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.End
+    ) {
+        CustomText(message = item.message)
+        Spacer(modifier = Modifier.size(8.dp))
+        Icon(Icons.Filled.AccountCircle, contentDescription = null)
+    }
+}
+
+@Composable
+fun MessageReceiveItem(item: Message) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(Icons.Filled.AccountCircle, contentDescription = null)
+        Spacer(modifier = Modifier.size(8.dp))
+        CustomText(message = item.message)
+    }
+}
+
+@Composable
+fun CustomText(message: String) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(4.dp)
+            )
+    ) {
+        Text(text = message, modifier = Modifier.padding(8.dp))
+    }
 }
 
 @Preview
@@ -145,87 +265,8 @@ fun PreTopBar() {
 @Preview
 @Composable
 fun PreMB() {
-    MessageInputBox(message = "Ask your question here")
-}
-
-@Composable
-fun MessageInputBox(
-    modifier: Modifier = Modifier.fillMaxSize(),
-    message: String,
-    onMessageChanged: (String) -> Unit = {}
-) {
-    Row(verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .height(56.dp)
-            .clickable {}
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .background(color = MaterialTheme.colorScheme.primary, shape = CircleShape)
-                .padding(
-                    horizontal = 16.dp,
-                )
-                .clickable {}
-        ) {
-            val textFieldColors = TextFieldDefaults.colors(
-                unfocusedTextColor = Color.LightGray,
-                focusedTextColor = Color.White,
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                cursorColor = Color.White
-            )
-            var text by remember { mutableStateOf(message) }
-            Icon(painterResource(id = R.drawable.tag_faces_black_24dp_1), null, tint = Color.White)
-            TextField(
-                value = text,
-                onValueChange = {
-                    text = it
-                },
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.title_hint),
-                        style = Typography.titleMedium
-                    )
-                },
-                textStyle = Typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                maxLines = 1,
-                colors = textFieldColors
-            )
-        }
-        Spacer(modifier = Modifier.size(8.dp))
-        Box(
-            modifier = Modifier
-                .background(
-                    color = MaterialTheme.colorScheme.primary,
-                    shape = CircleShape
-                )
-                .clickable {
-
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painterResource(id = R.drawable.ic_microphone),
-                null, tint = Color.White
-            )
-        }
-    }
-}
-
-@Composable
-fun MessageItem(messages: List<Message>) {
-    LazyColumn {
-        items(messages) { message ->
-            if(message.id == 1){
-                MessageReceiveItem(item = message)
-            }else{
-                MessageSendItem(item = message)
-            }
-        }
-    }
+    val m = stringResource(R.string.ask_your_question_here)
+    MessageInputBox(message = m)
 }
 
 @Preview
@@ -235,52 +276,16 @@ fun PreviewMessageItem() {
     MessageItem(messages = msg)
 }
 
-fun getDummyData() = listOf<Message>(
-    Message("Hello",5),
-    Message("Hello, how can I help you",1)
-)
-
+@Preview
 @Composable
-fun MessageSendItem(item: Message) {
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.End
-    ) {
-        Text(text = item.message)
-        Spacer(modifier = Modifier.size(8.dp))
-        Icon(Icons.Filled.AccountCircle, contentDescription = null)
-    }
+fun PreviewMessageReceiveItem() {
+    MessageReceiveItem(item = Message("Hello, how can I help you?", 2))
 }
 
 @Preview
 @Composable
 fun PreviewMessageSendItem() {
     MessageSendItem(item = Message("Hello", 1))
-}
-
-@Composable
-fun MessageReceiveItem(item: Message) {
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Icon(Icons.Filled.AccountCircle, contentDescription = null)
-        Spacer(modifier = Modifier.size(8.dp))
-        Text(text = item.message)
-    }
-}
-
-@Preview
-@Composable
-fun PreviewMessageReceiveItem() {
-    MessageReceiveItem(item = Message("Hello, how can I help you?", 2))
 }
 
 data class Message(val message: String, val id: Int)
